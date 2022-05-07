492e3fabbc995134967cb52e930e5a1a7d65c0e6
Justin Park <56710982+JustinParkCS@users.noreply.github.com>
2022-05-07 14:20:30 -0400

    feat: further developed cards to display data from json directly. Button functionality not yet implemented.

--- delivery-manager-client/src/components/DeliveryCard.js
+++ delivery-manager-client/src/components/DeliveryCard.js
@@ -1,7 +1,51 @@
 import React from "react";
-
+//{cardData.properties.name}
 const DeliveryCard = ({ cardData }) => {
-  return <div>{cardData.properties.name}</div>;
+  const handleEdit = () => {
+    console.log(`Edit button pressed for id: ${cardData.properties.id}.`);
+  };
+
+  const handleComplete = () => {
+    console.log(`Complete button pressed for id:${cardData.properties.id}.`);
+  };
+
+  return (
+    <div className="flex flex-col gap-y-2">
+      {/* Top row */}
+      <div className="flex flex-row justify-between gap-x-4">
+        {/* left side */}
+        <div>
+          <p>{cardData.properties.name}</p>
+        </div>
+        {/* right side */}
+        <div className="flex gap-x-6">
+          <button
+            className="border px-3 py-1 rounded-md bg-blue-600 text-white"
+            onClick={handleEdit}
+          >
+            Edit
+          </button>
+          <button
+            className="border px-3 py-1 rounded-md bg-green-600 text-white"
+            onClick={handleComplete}
+          >
+            Complete
+          </button>
+        </div>
+      </div>
+      {/* Bottom row */}
+      <div className="flex flex-row justify-between gap-x-4">
+        {/* left side */}
+        <div className="w-3/5 overflow-x-auto">
+          <p className="whitespace-nowrap">{cardData.properties.notes}</p>
+        </div>
+        {/* right side */}
+        <div>
+          <p>Distance: %DISTANCE% miles</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DeliveryCard;
